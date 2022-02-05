984244103c9f585cee04a7114f56c12af4920503
synw <synwx@protonmail.com>
2022-02-05 13:02:53 +0100

    Initial

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="{ dark: user.isDarkMode.value == true }">
+    <the-header></the-header>
+    <div class="p-3">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import TheHeader from "@/components/TheHeader.vue";
+import { user } from "@/state";
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+</style>
