68de41265039059831181be9c7ca4d2fa9cdd37d
synw <synwx@protonmail.com>
2022-08-10 15:06:27 +0200

    Minor update in templates

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -1,9 +1,5 @@
 <template>
-  <sw-sidebar
-    v-model:opened="sidebar"
-    name="sidebar1"
-    class="sw-sidebar flex flex-col h-full secondary"
-  >
+  <sw-sidebar v-model:opened="sidebar" name="sidebar1" class="flex flex-col h-full sw-sidebar secondary">
     <div class="flex-grow mt-5 ml-6 space-y-6">
       <div>
         <i-fluent-settings-32-regular class="txt-lighter dark:txt-light"></i-fluent-settings-32-regular>
@@ -12,10 +8,7 @@
         </span>
       </div>
     </div>
-    <div
-      class="flex-none h-12 mb-3 text-2xl text-center cursor-pointer"
-      @click="sidebar = !sidebar"
-    >
+    <div class="flex-none h-12 mb-3 text-2xl text-center cursor-pointer" @click="sidebar = !sidebar">
       <i-fa-solid:angle-double-left v-if="sidebar === true"></i-fa-solid:angle-double-left>
       <i-fa-solid:angle-double-right v-else></i-fa-solid:angle-double-right>
     </div>
@@ -24,18 +17,16 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
-import TheHeader from "@/components/TheHeader.vue";
 import SwSidebar from "@snowind/sidebar";
-import { user } from "@/state";
 
 const sidebar = ref(true);
 </script>
 
  <style lang="sass" scoped>
-
-#sidebar1
-  &.opened
-    @apply w-96
-</style>
+ 
+ #sidebar1
+   &.opened
+     @apply w-96
+ </style>
 
 
